bd24acba47044fa75239701b5243dbe9fb0bb7d4
Aleksander <trimadgic@proton.me>
2024-07-24 02:09:50 +0300

    test

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,5 +1,15 @@
+import { SanityImageSource } from "@sanity/image-url/lib/types/types"
+
 export interface SimpleBlogCard {
   title: string
   smallDescription: string
   currentSlug: string
+  titleImage: SanityImageSource
+}
+
+export interface FullBlog {
+  currentSlug: string
+  title: string
+  Content: any
+  titleImage: SanityImageSource
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { client } from "@/sanity/lib/client"
 import { SimpleBlogCard } from "./lib/interface"
+import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+import { urlFor } from "@/sanity/lib/image"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 async function getData() {
   const query = `
    *[_type == 'blog'] | order(_createdAt desc) {
      title,
        smallDescription,
-       "currentSlug": slug.current
+       "currentSlug": slug.current,
+       titleImage
    }
   `
   const data = await client.fetch(query)
@@ -18,9 +24,28 @@ export default async function Home() {
   const data: SimpleBlogCard[] = await getData()
   console.log(data)
   return (
-    <div>
-      {data.map(({ title }) => (
-        <span key={title}>{title}</span>
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 mb-5 gap-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image
+            key={idx}
+            src={urlFor(post.titleImage).url()}
+            alt={post.title}
+            width={500}
+            height={500}
+            className="rounded-t-lg h-[200px] w-full object-cover"
+          />
+
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Читать</Link>
+            </Button>
+          </CardContent>
+        </Card>
       ))}
     </div>
   )
